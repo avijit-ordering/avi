1c9f42b55774ff83d3a43c8765e7154e435bfaf3
Avijit Jana <avi@ordering.agency>
2023-11-03 14:41:48 +0530

    splash sceen module

--- src/navigators/RootNavigator.tsx
+++ src/navigators/RootNavigator.tsx
@@ -10,20 +10,26 @@ import {
   View,
 } from 'react-native';
 import Splash from '../pages/Splash/Splash';
-
+import SplashScreen from 'react-native-splash-screen';
 import { createStackNavigator } from '@react-navigation/stack';
 const Stack = createStackNavigator();
+
+
 const RootNavigator = () => {
-  
+  useEffect(()=>{
+    SplashScreen.hide();
+  },[])
 
   return (
     <View>
-      <Text>kol</Text>
+      <Text>hello</Text>
+      {/* <Stack.Navigator>
       <Stack.Screen
           name="Splash"
           component={Splash}
           options={{ headerShown: false }}
         />
+      </Stack.Navigator> */}
     </View>
   );
 };
